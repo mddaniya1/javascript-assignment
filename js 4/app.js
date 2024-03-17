7c9752d2791a08bd995c7fc07eefe940a3ab64a0
Md, Daniyal <151908847+mddaniya1@users.noreply.github.com>
2024-03-17 23:33:15 +0500

    Add files via upload

---
+++ js 4/app.js
@@ -0,0 +1,19 @@
+let myBikeName = "Honda", myCarName = "civic", name = "anas raza";
+
+alert(myBikeName+" "+myCarName+" "+name);
+
+
+// Legal variable names:
+let myName = "anas raza";
+let $full_Name = "anas raza";
+let _myname = "anas raza";
+let myFirstName = "anas";
+let age = 18;
+
+
+// Illegal variable names:
+let 5number; 
+let my variable; 
+let my-variable; 
+let my*variable; 
+let let; 
