411f485f459a708e8b4cd9fee2646cd136515d61
Md, Daniyal <151908847+mddaniya1@users.noreply.github.com>
2024-03-27 00:03:01 +0500

    Add files via upload

---
+++ chap 6 & 9/app.js
@@ -0,0 +1,22 @@
+//  var number = 10;
+
+// var result1 = number * 2;
+// var result2 = number + 2;
+// var result3 = number - 2;
+// var result4 = number / 2;
+
+
+// document.write("<h2>Arithemetic Operation on " + number + ":</h2>" );
+// document.write("<p>" + number + "multiplied by 2 is" +result1 + ":</p>" );
+// document.write("<p>" + number +  );
+// document.write("<p>" + number +  );
+// document.write("<p>" + number + );
+
+
+var myName = prompt("please enter your name:");
+
+if (name !== null  && name !== "") {
+    alert("Hello, " + name + "! wlcome!");
+} else {
+    alert("Hello! Welcome!");
+}
